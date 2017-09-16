42968794d3d6a433f0aa07927e2757ad319d4bfc
unknown <bobbin.powerz@gmail.com>
2017-09-15 21:57:06 -0700

    finalized

--- app.js
+++ app.js
@@ -1,108 +1,98 @@
 $(document).ready(function() {
 
-	var topics = ["frenchie", "donkey", "kitten", "badger", "pig", "alligator", "elephant"];
-	     
-		function renderButtons() {
+//Creating an array to store values for the buttons created later
+var topics = ["Hot Dogs", "Pizza", "Icecream", "Popcorn", "Donuts", "Beer", "Gummi Bears", "Cotton Candy"];
+     // Wrapping button creation into a function
+	function renderButtons() {
+	$("#foodButtons").empty();
+		//Creating new buttons in a for loop
+		for (var i = 0; i < topics.length; i++) {
+		  var inBtn = $("<button>");
+		  inBtn.attr("data-name", topics[i]);
+		  inBtn.text(topics[i]);
+		  //Rendering newly created buttons into their div
+		  $("#foodButtons").append(inBtn);
+		}
+	};
 
-		$("#animalButtons").empty();
+//On-click Submit form to create new buttons 
+$("#add-food").on("click", function(event) {
 
-			for (var i = 0; i < topics.length; i++) {
-			 
-			  var inBtn = $("<button>");
-			 
-			
-			  inBtn.attr("data-name", topics[i]);
-			
-			  inBtn.text(topics[i]);
-		
-			  $("#animalButtons").append(inBtn);
-			}
-		};
+	//Preventing the form performing its default post call
+	event.preventDefault();
 
-		 $("#add-animal").on("click", function(event) {
-		 	event.preventDefault();
-
-		 	var added = $("#animal-input").val().trim();
-		 	topics.push(added);
-		 	console.log(topics);
-		 	renderButtons();
-		 	fetch();
-		 	$("#animal-form").find("input:text").val("");
-
-		  });
-		// inBtn.attr("data-name", added);
-		// // The movie from the textbox is then added to our array
-		// buttns.push(added);
-		// console.log(buttns);
-		// // calling renderButtons which handles the processing of our movie array
-		// renderButtons();
-		// $("#animal-form").find("input:text").val("");
-		// });
-
+	//adding user's input into the existing array and running button creation function
+	var added = $("#food-input").val().trim();
+	topics.push(added);
+	console.log(topics);
 	renderButtons();
 
+	//calling api function to make an api call on newly created buttons
+	fetch();
 
-	function fetch() {
+	//Clearing the input field in the form once submitted
+	$("#food-form").find("input:text").val("");
 
-		
+});
 
-		$("button").on("click", function() {
-			$("#gifs-here").empty();
+//Calling function to create first buttons
+renderButtons();
 
-		var animal = $(this).attr("data-name");
+//Wrapping an api call in a function
+function fetch() {
+	$("button").on("click", function() {
+		//clearing previously rendered gifs
+		$("#gifs-here").empty();
+		var food = $(this).attr("data-name");
 		console.log(this);
 
+		//assembling API call wiht link and q as well as limiting response to 10 items
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-		animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-
+		food + "&api_key=dc6zaTOxFJmzC&limit=10";
 			$.ajax({
 			url: queryURL,
 			method: "GET"
 			})
-			.done(function(response) {
-			console.log(queryURL);
-			console.log(response);
+		.done(function(response) {
+		console.log(queryURL);
+		console.log(response);
+
 			// storing the data from the AJAX request in the results variable
-				var results = response.data;
-					// Looping through each result item
-					for (var i = 0; i < results.length; i++) {
-					// Creating and storing a div tag
-					var animalDiv = $("<div>");
-					// Creating a paragraph tag with the result item's rating
-					var rated = $("<p>").text("Rating: " + results[i].rating);
-					// Creating and storing an image tag
-					var animalImage = $("<img>");
-					// Setting the src attribute of the image to a property pulled off the result item
-					animalImage.attr("src", results[i].images.fixed_height_still.url);
-					animalImage.attr("data-animate", results[i].images.fixed_height.url);
-					animalImage.attr("data-still", results[i].images.fixed_height_still.url);
-					animalImage.attr("data-state", "still");
+			var results = response.data;
+			// Looping through each result item
+			for (var i = 0; i < results.length; i++) {
+				// Creating and storing a div tag
+				var foodDiv = $("<div>");
+				// Creating a paragraph tag with the result item's rating
+				var rated = $("<p>").text("Rating: " + results[i].rating);
+				// Creating and storing an image tag
+				var foodImage = $("<img>");
+				// Setting the src and 3 data attributes for each image
+				foodImage.attr("src", results[i].images.fixed_height_still.url);
+				foodImage.attr("data-animate", results[i].images.fixed_height.url);
+				foodImage.attr("data-still", results[i].images.fixed_height_still.url);
+				foodImage.attr("data-state", "still");
+					// creating switch logic to replace link to animated if not and to still if animated
+					foodImage.on("click", function() {
+						var state = $(this).attr("data-state");
+						if (state === "still") {
+						$(this).attr("src", $(this).attr("data-animate"));
+						$(this).attr("data-state", "animate");
+						} else {
+							$(this).attr("src", $(this).attr("data-still"));
+							$(this).attr("data-state", "still");
+						};
+					});
+					foodDiv.append(rated);
+					foodDiv.append(foodImage);
+				//prepending ready gifs into their div container
+				$("#gifs-here").prepend(foodDiv);
 
-
-
-						animalImage.on("click", function() {
-							var state = $(this).attr("data-state");
-							if (state === "still") {
-							$(this).attr("src", $(this).attr("data-animate"));
-							$(this).attr("data-state", "animate");
-							} else {
-								$(this).attr("src", $(this).attr("data-still"));
-								$(this).attr("data-state", "still");
-								console.log('IT IS STILL');
-							};
-						});
-					// Appending the paragraph and image tag to the animalDiv
-					animalDiv.append(rated);
-					animalDiv.append(animalImage);
-					
-					$("#gifs-here").prepend(animalDiv);
-					}
-			});
-
+			}
 		});
 
-		// $("#gifs-here").html("");
-	};
+	});
+ };
 
 	fetch();
 
